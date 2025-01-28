8c14765707c7049b7d3e1a7c56eaca1cccfd433c
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-01-28 19:54:29 +0530

    Create I\p sum of 3 int.java

---
+++ I\p sum of 3 int.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class hello {
+    public static void main(String args[])
+     {
+        Scanner charlie=new Scanner(System.in);
+      int a=charlie.nextInt();
+      int b=charlie.nextInt();
+      int c=charlie.nextInt();
+      int d=a+b+c;
+      System.out.println(d);
+       }
+    }
+     
