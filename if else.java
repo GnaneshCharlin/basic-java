6bcf88006afa68a74152a350072721cd3b82fead
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-01-29 19:36:07 +0530

    Create if else.java

---
+++ if else.java
@@ -0,0 +1,19 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+      Scanner charlie=new Scanner(System.in);
+      int num1=charlie.nextInt();
+      int num2=charlie.nextInt();
+      if (num1>num2) 
+     {
+      System.out.println("number 1 is greater");
+      }
+      else{
+         System.out.println("number 2 is greater ");
+      }
+       }
+    }
+     
+      
