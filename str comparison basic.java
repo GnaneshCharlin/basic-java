5af147c95f32a9be342616e6efff713f5d5bf308
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-01-29 19:51:55 +0530

    Create str comparison basic.java

---
+++ str comparison basic.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+    String a= new String ("apple");
+    String b=new String("apple");
+    String c=b;
+    System.out.println(a.equals(c));
+     }
+    }
