031463fa64cdf0533a6ea3ed6afba0b7069a4935
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-01-29 19:59:31 +0530

    Create str comparison.java

---
+++ str comparison.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+    String a= new String ("apple");
+    String b=new String("apple");
+    
+    String a1= ("apple");
+    String b1= ("apple");
+    System.out.println(a==b);
+       //error(false)
+    System.out.println(a1==b1);
+       //true
+     }
+    }
