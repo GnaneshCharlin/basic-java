267165cac2056df59e0237c856fd4d23f3737783
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-01-29 19:30:13 +0530

    Create if\else basics.java

---
+++ if\else basics.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+      int num1=40;
+      int num2=50;
+      if (num1==num2) 
+     {
+      System.out.println("it is equal");
+      }
+      else{
+         System.out.println("not equal");
+      }
+       }
+    }
