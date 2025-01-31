4d3bd8c905f583e681146423b251518e6cf238e8
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-01-31 19:48:08 +0530

    Create if\else basics2.java

---
+++ if\else basics2.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+      Scanner charlie=new Scanner(System.in);
+    String RCB=charlie.nextLine();
+    
+    if (RCB .equals("win"))
+      System.out.println("eesala cup namadheyy");
+    
+else
+   System.out.println("no cup");
+ 
+     }
+    }
