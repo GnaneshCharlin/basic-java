4ab8019f31351c51fa5659cf6579ed1034aa9d91
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-02-01 18:17:36 +0530

    Create for loop basic.java

---
+++ for loop basic.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+      Scanner charlie=new Scanner(System.in);
+      String bot=charlie.nextLine();
+      for(int i=1;i<=10;i++)
+      System.out.println(bot);
+     }
+    }
+     
