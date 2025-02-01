5154adf021f21b7a0571622be079f91840338bc2
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-02-01 19:00:06 +0530

    Create count basic.java

---
+++ count basic.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+      int evencount = 0;
+      for(int i=1;i<=10;i++)
+      if (i%2==0) {
+      evencount=evencount+1;
+      }
+      System.out.println(evencount);
+    }
+    }
+     
+      
