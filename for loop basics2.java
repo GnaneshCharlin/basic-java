e2880a7d22c5334fe1f38fab9508ba5499eaa6ba
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-02-01 18:21:18 +0530

    Create for loop basics2.java

---
+++ for loop basics2.java
@@ -0,0 +1,10 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+      
+      for(int i=5;i>=1;i--)
+      System.out.println(i);
+     }
+    }
