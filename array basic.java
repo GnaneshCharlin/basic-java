0aef33452e4d1b7b4f55abb6a044d73a2c760a88
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-02-04 20:33:26 +0530

    Create array basic.java

---
+++ array basic.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello {
+    public static void main(String args[])
+     {
+         int[] marks=new int[5];
+         Scanner charlie=new Scanner(System.in);
+         marks[0]=charlie.nextInt();
+         marks[1]=charlie.nextInt();
+         marks[2]=charlie.nextInt();
+         marks[3]=charlie.nextInt();
+         marks[4]=charlie.nextInt();
+         System.out.println(marks[0]+marks[1]+marks[2]+marks[3]+marks[4] +" = total marks");
+    }
+    }
+     
