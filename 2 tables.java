2087531d8d14964f5904ed0e1222155298107c8b
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-02-07 19:48:40 +0530

    Create 2 tables.java

---
+++ 2 tables.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+class hello {
+    public static void main(String args[])
+     {
+      int num=2;
+      for(int i=1;i<=10;i++)
+      {
+        System.out.println(num+"X"+i+"="+(num*i));
+      }
+    }
+    }
