803b5a745149ec2ce65a419ab597d6cd06a158ca
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-02-21 11:10:37 +0530

    Create finding higher value in array.java

---
+++ finding higher value in array.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+ class hello{
+    public static void main(String[] args) {
+        int numbers[] = {5, 10, 15, 20, 25};
+        int max=numbers[0];
+        System.out.println("checking higher value:");
+        for (int i = 0; i < numbers.length; i++) {
+        }
+        System.out.println("largest value: "+max);
+    }
+}
