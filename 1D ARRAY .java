236cc888859cab174a48deef775b2a6d90788a5b
GnaneshCharlin <153986198+GnaneshCharlin@users.noreply.github.com>
2025-02-21 11:02:24 +0530

    Create 1D ARRAY .java

---
+++ 1D ARRAY .java
@@ -0,0 +1,10 @@
+import java.lang.System;
+ class hello{
+    public static void main(String[] args) {
+        int numbers[] = {5, 10, 15, 20, 25};
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
